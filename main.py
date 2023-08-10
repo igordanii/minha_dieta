644d08c02d72a7bde582bd4d9eb422bc9e8d42c7
Igor Alencar <igordanice@gmail.com>
2023-08-10 00:39:10 -0300

    adding basic elements and fucnionalities

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
+
+@app.route("/")
+def load_index():
+   return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
