229f519fb40d65254d17f685598032b70914e750
Igor Alencar <igordanice@gmail.com>
2023-08-14 17:53:13 -0300

    complete success page

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def load_index():
-   return render_template("index.html")
+   if request.method == "POST":
+       return render_template("successful-submission.html")
+   else:
+      return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
